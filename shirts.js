20a9c9d4a354bc5bd51f1ff0bb983bb56e8dc5cc
seanHolahan <holahan.s@husky.neu.edu>
2017-04-06 23:25:01 -0400

    first commit, to-do zoom

---
+++ shirts.js
@@ -0,0 +1,327 @@
+var shirts_json = {
+    "type": "FeatureCollection",
+    "features":
+        [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.106002,42.34226 ]
+                },
+                "properties": {
+                    "Organization":"Wheelock College",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.100606,42.339067 ]
+                },
+                "properties": {
+                    "Organization":"Simmons College",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.08915,42.339823 ]
+                },
+                "properties": {
+                    "Organization":"Northeastern University",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.088364,42.330891 ]
+                },
+                "properties": {
+                    "Organization":"Boston Public Health Commission",
+                    "Category":"Health"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.089949,42.334437 ]
+                },
+                "properties": {
+                    "Organization":"Boston Police Department",
+                    "Category":"Safety"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.079886,42.346459 ]
+                },
+                "properties": {
+                    "Organization":"Neighborhood Works",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.071981,42.290812 ]
+                },
+                "properties": {
+                    "Organization":"Codman Square NDC",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.070507,42.350524 ]
+                },
+                "properties": {
+                    "Organization":"Boston Foundation",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.068821,42.351521 ]
+                },
+                "properties": {
+                    "Organization":"Clear Channel",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.081615,42.317903 ]
+                },
+                "properties": {
+                    "Organization":"Teen Empowerment",
+                    "Category":"Youth Services"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.083707,42.326834 ]
+                },
+                "properties": {
+                    "Organization":"Roxbury Boys and Girsl and Club",
+                    "Category":"Youth Services"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.072742,42.322582 ]
+                },
+                "properties": {
+                    "Organization":"Children Services of Roxbury",
+                    "Category":"Youth Services"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.05794,42.360184 ]
+                },
+                "properties": {
+                    "Organization":"Mayors Office ",
+                    "Category":"Government"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.060858,42.36163 ]
+                },
+                "properties": {
+                    "Organization":"Suffolk DA Office",
+                    "Category":"Government"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.058305,42.360316 ]
+                },
+                "properties": {
+                    "Organization":"City Council ",
+                    "Category":"Government"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.063821,42.358793 ]
+                },
+                "properties": {
+                    "Organization":"State House",
+                    "Category":"Government"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.062446,42.354831 ]
+                },
+                "properties": {
+                    "Organization":"Justice Resource Institute",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.087936,42.323673 ]
+                },
+                "properties": {
+                    "Organization":"City on a Hill",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.090307,42.329402 ]
+                },
+                "properties": {
+                    "Organization":"Rosies Place",
+                    "Category":"Safety"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.072087,42.289367 ]
+                },
+                "properties": {
+                    "Organization":"Codman Academy",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.090889,42.276409 ]
+                },
+                "properties": {
+                    "Organization":"Mildred Avenue School",
+                    "Category":"Education"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.062128,42.352733 ]
+                },
+                "properties": {
+                    "Organization":"Mass Commission for the Blind",
+                    "Category":"Health"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.062225,42.359627 ]
+                },
+                "properties": {
+                    "Organization":"MOVA",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.061947,42.3528 ]
+                },
+                "properties": {
+                    "Organization":"DTA",
+                    "Category":"Government"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.08355,42.336445 ]
+                },
+                "properties": {
+                    "Organization":"Dimmock Community Health Center",
+                    "Category":"Health"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.098004,42.319571 ]
+                },
+                "properties": {
+                    "Organization":"My Sisters House",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.14129,42.361525 ]
+                },
+                "properties": {
+                    "Organization":"Bridge Over Trouble Waters",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.070657,42.317422 ]
+                },
+                "properties": {
+                    "Organization":"Project Hope",
+                    "Category":"Community Outreach"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [ -71.12964,42.301166 ]
+                },
+                "properties": {
+                    "Organization":"Brighman and Women",
+                    "Category":"Health"
+                }
+            }
+        ]
+};
+
+
